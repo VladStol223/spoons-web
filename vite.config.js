de145231e0cd82717a5523894fc4f837f23dd096
VladStol223 <vlad.stol223@gmail.com>
2026-01-04 16:33:59 -0500

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA({ registerType: "autoUpdate", manifest: { name: "Spoons", short_name: "Spoons", start_url: "/", scope: "/", display: "standalone", theme_color: "#2c3a1b", background_color: "#2c3a1b", icons: [{ src: "/pwa-192.png", sizes: "192x192", type: "image/png" }, { src: "/pwa-512.png", sizes: "512x512", type: "image/png" }, { src: "/maskable-512.png", sizes: "512x512", type: "image/png", purpose: "maskable" }] } })]
+});
