17c08c440fdfec0cd92c3809508eec2cd7a7ae28
VladStol223 <vlad.stol223@gmail.com>
2026-01-04 19:19:17 -0500

    fixing login

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./App";
+import { AuthProvider } from "./auth/AuthProvider";
 import "./styles/app.css";
-import { registerSW } from "virtual:pwa-register";
 
-registerSW({ immediate: true });
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><BrowserRouter><AuthProvider><App /></AuthProvider></BrowserRouter></React.StrictMode>);
