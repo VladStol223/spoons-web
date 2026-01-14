6b358a27f0b65d1d4a6ef4243d17bfec625e142c
VladStol223 <vlad.stol223@gmail.com>
2026-01-13 21:49:24 -0500

    tryna remove this damn bar underneath the hub buttons!

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,6 @@ import App from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
 import { ThemeProvider } from "./theme/ThemeProvider";
 import "./styles/app.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><BrowserRouter><ThemeProvider><AuthProvider><App /></AuthProvider></ThemeProvider></BrowserRouter></React.StrictMode>);
